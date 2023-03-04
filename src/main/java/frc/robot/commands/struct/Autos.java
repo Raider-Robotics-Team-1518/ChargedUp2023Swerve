7835cc4ac550d9741ebbe021f45bc16566abb6b2
FRCTeam1518Student1 <skypantherstudios+frcstudent1@gmail.com>
2023-03-04 14:49:34 -0500

    moved auto class

--- src/main/java/frc/robot/commands/struct/Autos.java
+++ src/main/java/frc/robot/commands/struct/Autos.java
@@ -1,16 +1,35 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands.struct;
 
-public final class Autos {
-  /* Example static factory for an autonomous command.
-  public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
-    return Commands.sequence(subsystem.exampleMethodCommand()/*, command2, command3, command4, .....);
-  } */
+import java.util.List;
 
-  private Autos() {
-    throw new UnsupportedOperationException("This is a utility class!");
-  }
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.auto.PIDConstants;
+import com.pathplanner.lib.auto.SwerveAutoBuilder;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.base.SwerveDrive;
+
+public class Autos {
+    public static Command getFullAuto(String pathName, double maxVel, double maxAccel) {
+        List<PathPlannerTrajectory> pathGroup;
+        pathGroup = PathPlanner.loadPathGroup(pathName, new PathConstraints(maxVel, maxAccel));
+
+        SwerveDrive driveSystem = RobotContainer.swerveDrive;
+
+        SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
+            driveSystem::getCurPose2d,
+            driveSystem::setCurPose2d,
+            driveSystem.driveKinematics,
+            new PIDConstants(Constants.SWERVE_DRIVE_P_VALUE, Constants.SWERVE_DRIVE_I_VALUE, Constants.SWERVE_DRIVE_D_VALUE), 
+            new PIDConstants(Constants.SWERVE_ROT_P_VALUE, Constants.SWERVE_ROT_I_VALUE, Constants.SWERVE_ROT_D_VALUE),
+            driveSystem::setModuleStates,
+            Constants.autonomousEventMap,
+            true,
+            driveSystem);
+        return autoBuilder.fullAuto(pathGroup);
+    }
 }
